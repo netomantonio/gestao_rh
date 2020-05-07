580c95028bf2b347f50cc7651d258065d92a97c3
Antonio Miranda Neto <antoniomnjobs@gmail.com>
2020-05-07 17:08:55 -0300

    include ip for aws in setting

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '#d5(9q^#goc8zj#*@6_85x&pf0v4&5o(+uf(-ua&_kpyg&xztm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.23.115.79']
 
 
 # Application definition
