960f676e795ece4f926951cf16078ba20556d1de
Antonio Miranda Neto <antoniomnjobs@gmail.com>
2020-05-11 20:41:07 -0300

    create class Render end class Pdf

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,8 +1,12 @@
 import io
 
+from django.template.loader import get_template
+import xhtml2pdf.pisa as pisa
+
 from django.contrib.auth.models import User
 from django.http import HttpResponse
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from reportlab.pdfgen import canvas
 
@@ -11,16 +15,17 @@ from apps.funcionarios.models import Funcionario
 
 class FuncionarioCreate(CreateView):
     model = Funcionario
-    fields = ['nome', 'departamentos',]
+    fields = ['nome', 'departamentos', ]
 
     def form_valid(self, form):
         funcionario = form.save(commit=False)
         username = funcionario.nome.lower().split(' ')[0] + funcionario.nome.lower().split(' ')[1]
         funcionario.user = User.objects.create(
             username=username)
-        funcionario.empresa=self.request.user.funcionario.empresa
+        funcionario.empresa = self.request.user.funcionario.empresa
         funcionario.save()
         return super(FuncionarioCreate, self).form_valid(form)
+
 
 class FuncionarioList(ListView):
     model = Funcionario
@@ -28,6 +33,7 @@ class FuncionarioList(ListView):
     def get_queryset(self):
         empresa_funcionario = self.request.user.funcionario.empresa
         return Funcionario.objects.filter(empresa=empresa_funcionario)
+
 
 class FuncionarioEdit(UpdateView):
     model = Funcionario
@@ -58,4 +64,34 @@ def pdf_reportlab(request):
     buffer.close()
     response.write(pdf)
     return response
+
+
+class Render:
+
+    @staticmethod
+    def render(path: str, params: dict, filename: str):
+        template = get_template(path)
+        html = template.render(params)
+        response = io.BytesIO()
+        pdf = pisa.pisaDocument(
+            io.BytesIO(html.encode("UTF-8")), response)
+        if not pdf.err:
+            response = HttpResponse(
+                response.getvalue(), content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment;filename=%s.pdf' % filename
+            return response
+        else:
+            return HttpResponse("Error Rendering PDF", status=400)
+
+
+class Pdf(View):
+
+    def get(self, request):
+
+        params = {
+            'today': 'Var today',
+            'sales': 'Var sales',
+            'request': request,
+        }
+        return Render.render('funcionarios/relatorio.html', params, 'mylife')
 
