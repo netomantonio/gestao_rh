ae0ee670f3be82cd4e69b29cf13de1edaf7bb5a1
Antonio Miranda Neto <antoniomnjobs@gmail.com>
2020-05-13 13:21:52 -0300

    include rest_framework TokenAuthentication in api's view reg_hora_extra

--- apps/reg_hora_extra/api/views.py
+++ apps/reg_hora_extra/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, permissions
+from rest_framework.authentication import TokenAuthentication
 
 from apps.reg_hora_extra.api.serializers import HoraExtraSerializer
 from apps.reg_hora_extra.models import HoraExtra
@@ -7,5 +8,6 @@ from apps.reg_hora_extra.models import HoraExtra
 class HoraExtraViewSet(viewsets.ModelViewSet):
     queryset = HoraExtra.objects.all()
     serializer_class = HoraExtraSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
