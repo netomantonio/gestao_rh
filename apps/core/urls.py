dfb1556170febf732f576cc5ef67c144cdc4f7be
Antonio Miranda Neto <antoniomnjobs@gmail.com>
2020-05-13 16:48:09 -0300

    create route for celery

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from apps.core.views import home
+from apps.core.views import home, celery
 
 urlpatterns = [
     path('', home, name='home'),
+    path('celery/', celery, name='celery'),
 ]
